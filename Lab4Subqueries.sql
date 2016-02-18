cb8a2d7c6e9f22a048a8b57f966c020e8d0db18d
John <John.Randis1@Marist.edu>
2016-02-17 20:11:38 -0500

    Randis Lab 4 submission 2/17/2016

---
+++ Lab4Subqueries.sql
@@ -0,0 +1,266 @@
+﻿----------------------------------------------------------------------------------------
+-- Postgres create, load, and query script for CAP2.
+--
+-- SQL statements for the CAP2 database
+-- 
+-- Derived from the CAP examples in _Database Principles, Programming, and Performance_, 
+--   Second Edition by Patrick O'Neil and Elizabeth O'Neil
+-- 
+-- Modified by Pablo Rivas from an earlier edition of Alan G. Labouseur
+-- 
+-- Tested on Postgres 9.3.2    (For earlier versions, you may need
+--   to remove the "if exists" clause from the DROP TABLE commands.)
+----------------------------------------------------------------------------------------
+
+-- Connect to your Postgres server and set the active database to CAP2.  Then . . .
+
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS Agents;
+DROP TABLE IF EXISTS products;
+
+
+-- Customers --
+CREATE TABLE customers (
+  cid      char(4) not null,
+  name     text,
+  city     text,
+  discount decimal(5,2),
+ primary key(cid)
+);
+
+
+-- Agents --
+CREATE TABLE agents (
+  aid     char(3) not null,
+  name    text,
+  city    text,
+  percent real,
+ primary key(aid)
+);        
+
+
+-- Products --
+CREATE TABLE products (
+  pid      char(3) not null,
+  name     text,
+  city     text,
+  quantity integer,
+  priceUSD numeric(10,2),
+ primary key(pid)
+);
+
+
+-- Orders -- 
+CREATE TABLE orders (
+  ordno   integer not null,
+  mon     char(3),    
+  cid     char(4) not null references customers(cid),
+  aid     char(3) not null references agents(aid),
+  pid     char(3) not null references products(pid),
+  qty     integer,
+  dollars numeric(12,2),
+ primary key(ordno)
+);
+
+
+
+-- SQL statements for loading example data into the CAP2 database
+-- Tested on Postgres 9.3.2
+-- Connect to your Postgres server and set the active database to CAP2.  Then . . .
+
+-- Customers --
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c001', 'Tiptop', 'Duluth', 10.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c002', 'Basics', 'Dallas', 12.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c003', 'Allied', 'Dallas', 8.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c004' ,'ACME' ,'Duluth', 8.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c005' ,'Weyland-Yutani', 'Acheron', 0.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c006' ,'ACME' ,'Kyoto' ,0.00);
+
+
+-- Agents --
+INSERT INTO Agents( aid, name, city, percent )
+  VALUES('a01', 'Smith', 'New York', 6 );
+
+INSERT INTO Agents( aid, name, city, percent )
+  VALUES('a02', 'Jones', 'Newark', 6 );
+
+INSERT INTO Agents( aid, name, city, percent )
+  VALUES('a03', 'Brown', 'Tokyo', 7 );
+
+INSERT INTO Agents( aid, name, city, percent )
+  VALUES('a04', 'Gray', 'New York' ,6 );
+
+INSERT INTO Agents( aid, name, city, percent )
+  VALUES('a05', 'Otasi', 'Duluth', 5 );
+
+INSERT INTO Agents( aid, name, city, percent )
+  VALUES('a06', 'Smith', 'Dallas', 5 );
+
+INSERT INTO Agents( aid, name, city, percent )
+  VALUES('a08', 'Bond', 'London', 7 );
+
+
+-- Products --
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p01', 'comb', 'Dallas', 111400, 0.50 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p02', 'brush', 'Newark', 203000, 0.50 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p03', 'razor', 'Duluth', 150600, 1.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p04', 'pen', 'Duluth', 125300, 1.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p05', 'pencil', 'Dallas', 221400, 1.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p06', 'folder','Dallas', 123100, 2.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p07', 'case', 'Newark', 100500, 1.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p08', 'clip', 'Newark', 200600, 1.25 );
+
+
+-- Orders --
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1011, 'jan', 'c001', 'a01', 'p01', 1000, 450.00);
+
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1013, 'jan', 'c002', 'a03', 'p03', 1000, 880.00);
+
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1015, 'jan', 'c003', 'a03', 'p05', 1200, 1104.00);
+
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1016, 'jan', 'c006', 'a01', 'p01', 1000, 500.00);
+  
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1017, 'feb', 'c001', 'a06', 'p03',  600, 540.00);
+
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1018, 'feb', 'c001', 'a03', 'p04',  600, 540.00);
+
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1019, 'feb', 'c001', 'a02', 'p02',  400, 180.00);
+
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1020, 'feb', 'c006', 'a03', 'p07',  600, 600.00);
+  
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1021, 'feb', 'c004', 'a06', 'p01', 1000, 460.00);
+
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1022, 'mar', 'c001', 'a05', 'p06',  400, 720.00);
+    
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1023, 'mar', 'c001', 'a04', 'p05',  500, 450.00);
+
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1024, 'mar', 'c006', 'a06', 'p01',  800, 400.00);
+  
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1025, 'apr', 'c001', 'a05', 'p07',  800, 720.00);
+
+INSERT INTO Orders( ordno, mon, cid, aid, pid, qty, dollars )
+  VALUES(1026, 'may', 'c002', 'a05', 'p03',  800, 740.00);
+
+
+-- SQL statements for displaying example data into the CAP2 database
+-- Connect to your Postgres server and set the active database to CAP2.  Then . . .
+
+--1. 
+SELECT city
+FROM agents 
+WHERE aid in (
+	SELECT aid
+	FROM orders
+	WHERE cid = 'c002');
+
+--2.
+SELECT pid
+FROM orders 
+WHERE aid in(
+	SELECT aid
+	FROM orders
+	WHERE cid in(
+		SELECT cid
+		FROM customers
+		WHERE city = 'Dallas'))
+ORDER BY pid DESC;
+
+--3.
+SELECT cid, name
+FROM customers
+WHERE cid in(
+	SELECT cid
+	FROM orders
+	WHERE aid != 'a01');
+
+--4. 
+SELECT cid
+FROM customers
+WHERE cid in(
+	SELECT cid
+	FROM orders
+	WHERE pid = 'p01' 
+	OR pid = 'p07');
+
+--5.
+SELECT pid
+FROM products
+WHERE pid in(
+	SELECT pid 
+	FROM orders
+	WHERE aid != 'a07')
+ORDER BY pid DESC;
+
+--6.
+SELECT name, discount, city
+FROM customers
+WHERE cid in(
+	SELECT cid 
+	FROM orders
+	WHERE aid in(
+		SELECT aid 
+		FROM agents
+		WHERE city = 'London' 
+		OR city = 'New York'));
+
+--7.
+SELECT *
+FROM customers
+WHERE discount in(
+	SELECT discount
+	FROM customers
+	WHERE city = 'Dallas' 
+	OR city = 'London');
+
+--8.
+/*
+* A check contraint in SQL is used to restrict the type of data entered into a 
+* given field in a table. Check constrains are helpful for ensuring the user does
+* not enter invalid data. For instance, if you do not want the user to enter a
+* negative number in a num field, then you should put a check contraint to prevent
+* that. An example would be inputting stock of an item; you should not be allowed
+* to have negative stock of something. A bad example of this implementation would
+* be to disallow negative currency values. While typically you would not want to
+* have a negative monetary value, it may be neccessary to allow such data to be
+* added to account for instances when there is a negative net profit. 
+*/
